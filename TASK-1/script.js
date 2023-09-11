11df361bc5c43a333f8fe724df3d9ea9d121f969
raviburaga <109849640+raviburaga@users.noreply.github.com>
2023-09-11 16:15:56 +0530

    Temperature_Converter

---
+++ TASK-1/script.js
@@ -0,0 +1,21 @@
+var celsius = document.getElementById("celsius");
+var fahrenheit = document.getElementById("fahrenheit");
+
+
+function celsiusConv(){
+    let output = ( parseFloat(celsius.value) * 9/5 ) + 32;
+    
+    fahrenheit.value = parseFloat(output.toFixed(2));
+ 
+
+}
+
+function fahrenConv(){
+
+    let output = ( parseFloat(fahrenheit.value) - 32) * 5/9;
+    
+    celsius.value = parseFloat( output.toFixed(2));
+
+    console.log(output);
+
+}
